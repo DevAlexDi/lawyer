4fdc9b6c914439d6d27f31b56c3b7413098fdd74
keitmark <keitmark@mail.ru>
2019-06-06 19:12:58 +0300

    feat: add footer/scroll navbar/sublvl menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
+
+    $('.modal-request-show').click(function(){
+        $('#modal-request').modal('show');
+    });
+
+    $('.tel-inp').inputmask({
+        "mask": "+7 (999) 999-99-99"
+        , "placeholder": "_"
+        , showMaskOnHover: false
+        , showMaskOnFocus: true
+    });
+
+    function scrollMenus() {
+        if($(window).scrollTop() > 0){
+            $('header').addClass('fixed-style');
+          }
+          else {
+            $('header').removeClass('fixed-style');
+        }
+    }
    
+    scrollMenus();
+
+    $(window).scroll(function(){
+        scrollMenus();
+    });
     
 });
 
